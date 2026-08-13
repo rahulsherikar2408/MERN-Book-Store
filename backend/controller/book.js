3bda41221fd1c31310618c9551ac2e6cb2a58d04
rahulsherikar2408 <sherikarrahul59@gmail.com>
2026-08-13 10:57:18 +0530

    Adding get and update book routes

--- backend/controller/book.js
+++ backend/controller/book.js
@@ -36,3 +36,56 @@ export const addBook = async(req, res) => {
         res.status(500).json({ message: error.message });
     }
 }
+
+export const getBookDetail = async(req, res) => {
+    try {
+        const id = req.params.id;
+        const { title, author, publishYear } = req.body;
+        const book = await Book.findById(id);
+        if(!book){
+            return res.status(404).json({ message: "Book not found" });
+        }
+        return res.status(200).json({
+            message: "Book found",
+            book: book
+        });
+    } catch (error) {
+        console.log("Book Detail:", error.message);
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const updateBook = async(req, res) => {
+    try {
+        const id = req.params.id;
+        const { title, author, publishYear } = req.body;
+        if(!title || !author || !publishYear){
+            return res.status(400).json({
+                message: "Enter all required fields"
+            })
+        }
+
+        const result = await Book.findByIdAndUpdate(id, {
+            title,
+            author,
+            publishYear
+        });
+
+        if(!result){
+            return res.status(404).json({ message: "Book not found" });
+        }
+
+        return res.status(200).json({
+            message: "Book Updated Succefully",
+            book: {
+                title,
+                author,
+                publishYear
+            }
+        });
+
+    } catch (error) {
+        console.log("Update Book:", error.message);
+        res.status(500).json({ message: error.message });
+    }
+}
--- backend/routes/bookRoute.js
+++ backend/routes/bookRoute.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { addBook, getBook } from '../controller/book.js';
+import { addBook, getBook, getBookDetail, updateBook } from '../controller/book.js';
 
 const router = express.Router();
 
 router.get('/list', getBook);
 
 router.post('/add', addBook);
+
+router.get('/detail/:id', getBookDetail);
+
+router.put('update/:id', updateBook);
 
 export default router;
